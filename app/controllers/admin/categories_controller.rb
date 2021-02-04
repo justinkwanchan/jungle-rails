f20be10f86c347e58e605c2f1ec60387f56e4731
justinkwanchan <justinkwanchan@gmail.com>
2021-02-04 19:52:33 +0000

    Add route, view, and controller for Admin > Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASS']
+  
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+  
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+end
